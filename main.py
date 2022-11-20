440a481215d594121098db182c4b27c39201fb9c
Emirit789 <118674519+Emirit789@users.noreply.github.com>
2022-11-20 23:35:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -82,8 +82,8 @@ class Punch:
             try:
                 res = requests.post(url, json=data, headers=headers, timeout=30)
                 if res.status_code == 200:
-                    return "打卡成功"
                     self.wechatNotice("打卡成功")
+                    return "打卡成功"
                 elif retryCnt == 3:
                     print("提交表单失败")
                     self.wechatNotice("打卡失败")
